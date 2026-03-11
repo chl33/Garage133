daa13aa905e16bbb84df84205636a2573ede8953
Chris Lee <lee.chris.h@gmail.com>
2026-03-10 22:54:21 -0400

    remove commented-out obsolete fixes in build-svelte.sh

--- build-svelte.sh
+++ build-svelte.sh
@@ -4,8 +4,3 @@ here="$(readlink -f "$(dirname "$0")")"
 cd "$here/svelte"
 npm run build
 npx svelteesp32 -e psychic -s dist -o ../src/svelteesp32async.h --etag=true
-# Use og3::WebServer wrapper
-#sed -i 's/void initSvelteStaticFiles(PsychicHttpServer \* server)/void initSvelteStaticFiles(og3::WebServer \* web_server)/' ../lib/watering/svelteesp32async.h
-# Call native_server() directly
-#sed -i 's/server->on/web_server->native_server().on/g' ../lib/watering/svelteesp32async.h
-#sed -i 's/server->defaultEndpoint/web_server->native_server().defaultEndpoint/g' ../lib/watering/svelteesp32async.h
