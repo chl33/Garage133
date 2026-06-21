fe653d0ba060587a42a459fd1aeb16c7783746c1
Chris Lee <lee.chris.h@gmail.com>
2026-06-20 20:07:12 -0400

    increase the delays between sonar readings

--- src/main.cpp
+++ src/main.cpp
@@ -411,11 +411,11 @@ void update() {
   s_left_sonar_2.setTemp(s_shtc3.temperature());
 
   s_left_sonar.read();
-  delay(2);  // Wait a short (2msec) time between sonar readings.
+  delay(4);  // Wait a short (2msec) time between sonar readings.
   s_right_sonar.read();
-  delay(2);
+  delay(4);
   s_left_sonar_2.read();
-  delay(2);
+  delay(4);
   s_right_sonar_2.read();
 
   char text[256];
